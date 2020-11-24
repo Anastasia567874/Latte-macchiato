59e2a2127690c9001cae7ef4d93ba3a70729fe7a
Admin <anastasia.vecherenko@yandex.ru>
2020-11-24 18:43:00 +0500

    Программа про круги

--- main.py
+++ main.py
@@ -1,3 +1,45 @@
-print('My first Git program')
-print('And i change it')
-print('Hi!!!!')
+import sys
+from window import Ui_MainWindow
+from random import randrange
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_rect(qp)
+            qp.end()
+
+    def draw_rect(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        for i in range(randrange(5, 30)):
+            r = randrange(10, 50)
+            x = randrange(100, 500)
+            y = randrange(100, 500)
+            qp.drawEllipse(x, y, r, r)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    form = MyWidget()
+    form.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
