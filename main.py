9fe4cd82e2e2b6f46f1d865fb2cba8d477e8fa3a
Admin <anastasia.vecherenko@yandex.ru>
2020-11-24 20:05:40 +0500

    Программа с возможностью просмотра базы данных

--- main.py
+++ main.py
@@ -1,76 +1,33 @@
 import sys
-from random import randrange
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+import sqlite3
 
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(600, 579)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(40, 30, 151, 41))
-        font = QtGui.QFont()
-        font.setPointSize(16)
-        font.setBold(True)
-        font.setWeight(75)
-        self.pushButton.setFont(font)
-        self.pushButton.setObjectName("pushButton")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 600, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "Нарисовать"))
-
-
-class MyWidget(QMainWindow, Ui_MainWindow):
+class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        self.do_paint = False
-        self.pushButton.clicked.connect(self.paint)
+        uic.loadUi('main.ui', self)
+        self.update_result()
 
-    def paint(self):
-        self.do_paint = True
-        self.repaint()
-
-    def paintEvent(self, event):
-        if self.do_paint:
-            qp = QPainter()
-            qp.begin(self)
-            self.draw_rect(qp)
-            qp.end()
-
-    def draw_rect(self, qp):
-        for i in range(randrange(5, 30)):
-            qp.setBrush(QColor(randrange(0, 255), randrange(0, 255), randrange(0, 255)))
-            r = randrange(10, 50)
-            x = randrange(100, 500)
-            y = randrange(100, 500)
-            qp.drawEllipse(x, y, r, r)
-
-
-def except_hook(cls, exception, traceback):
-    sys.__excepthook__(cls, exception, traceback)
+    def update_result(self):
+        con = sqlite3.connect("coffee.db")
+        cur = con.cursor()
+        result = cur.execute("""SELECT * FROM coffee""").fetchall()
+        self.tableWidget.setRowCount(len(result))
+        self.tableWidget.setColumnCount(len(result[0]))
+        titles = [description[0] for description in cur.description]
+        self.tableWidget.setHorizontalHeaderLabels(titles)
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+        con.close()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    form = MyWidget()
-    form.show()
-    sys.excepthook = except_hook
+    ex = MyWidget()
+    ex.show()
     sys.exit(app.exec())
+
